b3e20b2430d61785c1810f71942d9ab8f8130be9
CAMPSMITH <msmith92663@gmail.com>
2016-10-16 15:50:28 -0700

    added basic DNSSD

--- MSee/src/team4141/robotvision/msee/Main.java
+++ MSee/src/team4141/robotvision/msee/Main.java
@@ -7,13 +7,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("starting MSee server ... ");
-		Server server = new Server();
-		server
-			.add("frameRate", new IntegerConfigSetting(new Integer(1), new Integer(30), new Integer(20)))
-			.add("serverName", new StringConfigSetting("HolySee"))
-			.configure();
-		server.start();
-		while(server.isRunning()){
+//		Server server = new Server();
+//		server
+//			.add("frameRate", new IntegerConfigSetting(new Integer(1), new Integer(30), new Integer(20)))
+//			.add("serverName", new StringConfigSetting("HolySee"))
+//			.configure();
+//		server.start();
+
+	    new Thread(new RoboRioListener()) .start();
+	    
+//		while(server.isRunning()){
+		boolean done = false;
+		while(!done){
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
