c80f93fcbe78431401e61dbdc49630019dc74973
CAMPSMITH <CAMPSMITH@users.noreply.github.com>
2017-01-25 18:42:50 -0800

    refactored device discovery

--- MSee/src/team4141/robotvision/msee/Source.java
+++
@@ -1,8 +0,0 @@
-package team4141.MSee;
-
-public abstract class Source {
-	public abstract void add(Setting setting); 
-	public abstract String get(String settingName); 
-	public abstract String getName(); 
-	public abstract String match(); 
-}
